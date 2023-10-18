93d045527087e3d552c47e40eac956fd8b90e57e
Riley Anderson <rmanderson@wesleyan.edu>
2023-10-18 17:40:27 -0400

    Increasing the base size and creating an ellipse figure.

--- Analyses/nmds_plot_new.R
+++ Analyses/nmds_plot_new.R
@@ -3,11 +3,28 @@ library(vegan)
 library(ggplot2)
 library(ggpubr)
 library(tidyverse)
+
+
 # Load the dataset (example: compound data)
 chem <- read.csv("data/Mike_Trees_herbivores2.csv")
 
+
+# functions
+
+veganCovEllipse<-function (cov, center = c(0, 0), scale = 1, npoints = 100) 
+{
+  theta <- (0:npoints) * 2 * pi/npoints
+  Circle <- cbind(cos(theta), sin(theta))
+  t(center + scale * t(Circle %*% chol(cov)))
+}
+
+
+
+############################
+
 summary(chem)
-chem <- mutate(chem, treatment.1 = ifelse(treatment.1 == "minus", "removed", "replaced"))
+chem <- mutate(chem,
+               treatment.1 = ifelse(treatment.1 == "minus", "removed", "replaced"))
 # Perform NMDS with "treatment" as the grouping variable
 nmds <- metaMDS(chem[, 5:37], distance = "bray", k = 2)
 
@@ -16,7 +33,9 @@ nmds <- metaMDS(chem[, 5:37], distance = "bray", k = 2)
 # Add the "treatment" variable to the NMDS result
 nmds_data <- data.frame(nmds$points, treatment = chem$treatment, 
                         exclusion = chem$treatment.1, 
-                        taxon = chem$ploem.feeder)
+                        taxon = chem$ploem.feeder,
+                        sites = 1:16,
+                        row.names = "sites")
 
 # Calculate centroid for each treatment
 centroids <- nmds_data |> group_by(exclusion, taxon, treatment) |> 
@@ -28,14 +47,76 @@ centroids <- nmds_data |> group_by(exclusion, taxon, treatment) |>
   geom_point(size = 2, show.legend=FALSE) +
   geom_point(data = centroids, size = 4) +
   geom_segment(data = nmds_data,
-                aes(xend = centroids$MDS1[match(treatment, centroids$treatment)], 
-                    yend = centroids$MDS2[match(treatment, centroids$treatment)]), 
+                aes(xend = centroids$MDS1[match(treatment,
+                                                centroids$treatment)], 
+                    yend = centroids$MDS2[match(treatment,
+                                                centroids$treatment)]), 
                 linetype = "dashed") +
   labs(x = "NMDS1", y = "NMDS2", shape = "Treatment", color = "Taxon") +
   scale_shape_manual(values=c(1, 16)) +
-  theme_classic()
+  theme_classic(base_size = 20)
 
 # Display the NMDS plot
 print(plot_nmds)
 
 
+
+################################
+# ellipses
+nmds_data <- nmds_data %>% 
+  unite(treatment, c("exclusion", "taxon"))
+
+nmdsmeta <- data.frame(
+  MDS1 = nmds$points[,1],
+  MDS2 = nmds$points[,2],
+  group = nmds_data$treatment
+)
+
+nmdsmeta$group <- as.factor(nmdsmeta$group)
+
+
+ord <- ordiellipse(nmds,
+                   nmds_data$treatment,
+                   display = "sites",
+                   kind = "se", conf = 0.95, label = T)
+
+df_ell <- data.frame()
+for(g in levels(nmdsmeta$group)){
+  df_ell <- rbind(df_ell,
+                  cbind(
+                    as.data.frame(with(
+                      nmdsmeta[nmdsmeta$group==g,],
+                      veganCovEllipse(ord[[g]]$cov,ord[[g]]$center,ord[[g]]$scale)))
+                                ,group=g))
+}
+
+df_ell <- df_ell %>% 
+  mutate(exclusion = if_else(grepl("replaced", group), "replaced", "removed"),
+         taxon = if_else(grepl("membracid", group), "membracid", "coccid"))
+
+nmds_data <- nmds_data %>% 
+  separate_wider_delim(treatment, delim = "_",
+                       names = c("exclusion", "taxon"))
+
+#########################################
+# test
+
+ell.fig <- ggplot(nmds_data, aes(x = MDS1, y = MDS2, 
+                                   colour = taxon , shape = exclusion)) +
+  geom_point(size = 2, show.legend=FALSE) +
+  geom_point(data = centroids, size = 5) +
+  geom_path(data = df_ell,
+            aes(x = NMDS1, y = NMDS2)) +
+  # geom_segment(data = nmds_data,
+  #              aes(xend = centroids$MDS1[match(treatment,
+  #                                              centroids$treatment)], 
+  #                  yend = centroids$MDS2[match(treatment,
+  #                                              centroids$treatment)]), 
+  #              linetype = "dashed") +
+  labs(x = "NMDS1", y = "NMDS2", shape = "Treatment", color = "Taxon") +
+  scale_shape_manual(values=c(1, 16)) +
+  theme_classic(base_size = 20)
+
+######
+# print the fig
+print(ell.fig)
